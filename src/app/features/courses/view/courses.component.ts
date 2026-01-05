4081b71426672d4b6afed99f47aa7a04f84a0ecc
JamissonBarbosa <jamissondasilvafilho@gmail.com>
2026-01-05 16:33:06 -0300

    feat: create main layout

---
+++ src/app/features/courses/view/courses.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from "@angular/router";
+
+@Component({
+  selector: 'app-courses',
+  standalone: true,
+  imports: [],
+  templateUrl: './courses.component.html',
+  styleUrl: './courses.component.scss'
+})
+export class CoursesComponent {
+
+}
